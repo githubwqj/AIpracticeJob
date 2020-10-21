233847daa72b1c88845c85711c960925d1fb022b
githubwqj2 <2812930196@qq.com>
2020-10-21 23:25:12 +0800

    提交作业

---
+++ calc1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        double number = 0,number2=0,result=0;
+        int inputnumber;
+        bool imode = false,isequal=false;
+        enum Operate { none, plus, minus, mutiplication, division }
+
+        Operate mode = Operate.none;
+        public Form1()
+        {
+            InitializeComponent();
+            viewbefore.Text = Convert.ToString("");
+            viewmid.Text = Convert.ToString("");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            number = number * 10 + 1;
+            viewbase.Text = Convert.ToString(number);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            number = number * 10 + 9;
+            viewbase.Text = Convert.ToString(number);
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (mode != Operate.none) {
+            //等于===== /
+            if (mode == Operate.plus)
+            {
+                result = number2 + number;
+            }
+            if (mode == Operate.minus) {
+                result = number2 - number;
+            }
+            if (mode == Operate.mutiplication)
+            {
+                result = number2 * number;
+            }
+            if (mode == Operate.division)
+            {
+                result = number2 / number;
+            }
+            
+            cleanall();
+            number = result;
+            mode = Operate.none;
+            viewbase.Text = Convert.ToString(result);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            number = number * 10 + 2;
+            viewbase.Text = Convert.ToString(number);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            number = number * 10 + 3;
+            viewbase.Text = Convert.ToString(number);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            number = number * 10 + 4;
+            viewbase.Text = Convert.ToString(number);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            number = number * 10 + 5;
+            viewbase.Text = Convert.ToString(number);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            number = number * 10 + 6;
+            viewbase.Text = Convert.ToString(number);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            number = number * 10 + 7;
+            viewbase.Text = Convert.ToString(number);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            number = number * 10 + 8;
+            viewbase.Text = Convert.ToString(number);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            number = number * 10;
+            viewbase.Text = Convert.ToString(number);
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            //清除;/
+            number = 0;
+            viewbase.Text = Convert.ToString(0);
+            number2 = 0;
+            viewbefore.Text = Convert.ToString("");
+            viewmid.Text = Convert.ToString("");
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void call(int an) {
+            if (imode == false)
+            {
+                number = number * 10 + an;
+                viewbase.Text = Convert.ToString(number2);
+            }
+            else {
+                number2= number2 * 10 + an;
+                viewbase.Text = Convert.ToString(number2);
+            }
+        }
+        public void cleanall(){
+            if (!isequal) {
+                number = 0;
+                viewbase.Text = Convert.ToString(number);
+            }
+
+            viewbefore.Text = "";
+            viewmid.Text = "";
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //+++/
+            mode = Operate.plus;
+            viewmid.Text = "+";
+            number2 = number;
+            viewbefore.Text = Convert.ToString(number);
+            number = 0;
+            viewbase.Text = Convert.ToString(number);
+            imode = true;
+        }
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //+++/
+            mode = Operate.minus;
+            viewmid.Text = "-";
+            number2 = number;
+            viewbefore.Text = Convert.ToString(number);
+            number = 0;
+            viewbase.Text = Convert.ToString(number);
+            imode = true;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //***/
+            mode = Operate.mutiplication;
+            viewmid.Text = "*";
+            number2 = number;
+            viewbefore.Text = Convert.ToString(number);
+            number = 0;
+            viewbase.Text = Convert.ToString(number);
+            imode = true;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        { 
+            //"///"/
+            mode = Operate.division;
+            viewmid.Text = "/";
+            number2 = number;
+            viewbefore.Text = Convert.ToString(number);
+            number = 0;
+            viewbase.Text = Convert.ToString(number);
+            imode = true;
+
+        }
+
+        private void viewbefore_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ speechRecognition/speechRecognition/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using Baidu.Aip.Speech;
+using Microsoft.Win32;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace speechRecognition
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("winmm.dll", SetLastError = true)]
+        static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+
+        string appId = "22836516";
+        string apiKey = "XjN1hGG9pVcBtiOef4sGREAw";
+        string secretKey = "aD7p9zx2ung6nZccvmtUXcEPYWzQ367v";
+        string openfilePath = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        //选择文件
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog()
+            {
+                Filter = "files|*.wav;*.pcm;*.amr;",
+                FilterIndex = 1,
+                Multiselect = false
+            };
+
+            if (dialog.ShowDialog().GetValueOrDefault())
+            {
+                openfilePath = dialog.FileName;
+                mp3Link.Text = openfilePath;
+            }
+        }
+
+        //音频识别
+        private void startRec_Click(object sender, RoutedEventArgs e)
+        {
+            recognizeResult.Text = "";
+            Asr asr = new Asr(appId, apiKey, secretKey);
+            if (openfilePath == "")
+            {
+                recognizeResult.Text = "先选取文件";
+                return;
+            }
+            if (!File.Exists(openfilePath))
+            {
+                return;
+            }
+            FileInfo fileinfo = new FileInfo(openfilePath);
+            byte[] data = new byte[fileinfo.Length];
+            FileStream fs = fileinfo.OpenRead();
+            fs.Read(data, 0, Convert.ToInt32(fs.Length));
+            fs.Close();
+            string selectMode = comboBox.Text.ToString();
+            JObject obj = asr.Recognize(data, selectMode, 16000);
+            if (obj["err_msg"].ToString() != "success.")
+            {
+                recognizeResult.Text = obj["err_msg"].ToString();
+                return;
+            }
+            Array output_ratio = obj["result"].ToArray();
+            string ss = output_ratio.GetValue(0).ToString();
+            recognizeResult.Text = ss;
+        }
+
+        //音频合成
+        private void startCreate_Click(object sender, RoutedEventArgs e)
+        {
+            string text = textInput.Text;
+            Tts tts = new Tts(apiKey, secretKey);
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add("aue", "6");
+            TtsResponse result = tts.Synthesis(text, dic);
+            if (!result.Success)
+            {
+                MessageBox.Show("合成失败");
+                return;
+            }
+            Byte[] data = result.Data;
+            string filePath = System.IO.Path.Combine(Environment.CurrentDirectory, @"result.wav");
+            Bytes2File(data, filePath);
+            // 播放音频文件
+            mciSendString("open result.wav alias temp_alias", null, 0, IntPtr.Zero);
+            mciSendString("play temp_alias", null, 0, IntPtr.Zero);
+            // 等待播放结束
+            StringBuilder strReturn = new StringBuilder(64);
+            do
+            {
+                mciSendString("status temp_alias mode", strReturn, 64, IntPtr.Zero);
+            } while (!strReturn.ToString().Contains("stopped"));
+            // 关闭音频文件
+            mciSendString("close temp_alias", null, 0, IntPtr.Zero);
+        }
+
+        //生成文件
+        public static void Bytes2File(byte[] data, string savepath)
+        {
+            if (File.Exists(savepath))
+            {
+                File.Delete(savepath);
+            }
+            FileStream fs = new FileStream(savepath, FileMode.CreateNew);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(data, 0, data.Length);
+            bw.Close();
+            fs.Close();
+        }
+    }
+}
