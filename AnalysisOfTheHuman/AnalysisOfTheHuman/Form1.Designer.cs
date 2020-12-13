dfbe55a10ab72a24d9b2020892a99e636445f6d8
githubwqj <893453172@qq.com>
2020-12-14 00:13:00 +0800

    111

---
+++ AnalysisOfTheHuman/AnalysisOfTheHuman/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace AnalysisOfTheHuman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "关键点识别";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(278, 369);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(311, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "人流量统计";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox2.Location = new System.Drawing.Point(311, 52);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(361, 134);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(311, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "手势识别";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(311, 232);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(208, 189);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(448, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "人流量:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(525, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(534, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "手势识别结果";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(537, 253);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(135, 168);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 433);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "人体分析程序";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ AnalysisOfTheHuman/AnalysisOfTheHuman/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace AnalysisOfTheHuman
+{
+    public partial class Form1 : Form
+    {
+        static String AppID = "23142978";
+        static String APIKEY = "eUb5ZP9oQfyrcbGD4iHVNARo";
+        static String SECRETKEY = "3aZl2tCRFQCPfe0UqHOCojddrwRr3ACR";
+        String filepath1 = "";
+        static Baidu.Aip.BodyAnalysis.Body client = null;
+        public Form1()
+        {
+            InitializeComponent();
+            client = new Baidu.Aip.BodyAnalysis.Body(APIKEY, SECRETKEY);
+            client.Timeout = 60000;  // 修改超时时间
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.InitialDirectory = ".";
+            file.Filter = "所有文件(*.*)|*.*";
+            file.ShowDialog();
+            
+            filepath1 = file.FileName;   //获得文件的绝对路径
+            if (filepath1 == null || filepath1 == "")
+                return;
+            FileStream fs = new FileStream(filepath1, FileMode.Open, FileAccess.Read); //将图片以文件流的形式进行保存
+            BinaryReader br = new BinaryReader(fs);
+            byte[] imgBytesIn = br.ReadBytes((int)fs.Length); //将流读入到字节数组中 二进制数组
+            MemoryStream ms = new MemoryStream(imgBytesIn);
+            pictureBox1.Image = Image.FromStream(ms);
+            var image = File.ReadAllBytes(filepath1);
+            var result = client.BodyAnalysis(image);
+            var person_pix=(JObject)((JArray)result["person_info"])[0];
+            Graphics g = pictureBox1.CreateGraphics();//创建一个画板
+            IEnumerable<JProperty> properties = person_pix.Properties();
+            g.FillEllipse(Brushes.Red, 10, 10, 4, 4);
+            var b = person_pix.PropertyValues();
+            var c = person_pix.HasValues;
+            foreach (JToken item in properties)
+            {
+                while (item.HasValues) { 
+               var value = (JObject)item.Next;
+               g.FillEllipse(Brushes.Red, Convert.ToInt32(value["x"]), Convert.ToInt32(value["y"]), 4, 4);
+                }
+            }
+            
+                
+            Console.WriteLine(result); 
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.InitialDirectory = ".";
+            file.Filter = "所有文件(*.*)|*.*";
+            file.ShowDialog();
+            
+            filepath1 = file.FileName;   //获得文件的绝对路径
+            if (filepath1 == null || filepath1 == "")
+                return;
+            FileStream fs = new FileStream(filepath1, FileMode.Open, FileAccess.Read); //将图片以文件流的形式进行保存
+            BinaryReader br = new BinaryReader(fs);
+            byte[] imgBytesIn = br.ReadBytes((int)fs.Length); //将流读入到字节数组中 二进制数组
+            MemoryStream ms = new MemoryStream(imgBytesIn);
+            pictureBox2.Image = Image.FromStream(ms);
+            var image = File.ReadAllBytes(filepath1);
+            var options = new Dictionary<string, object>{
+    	    {"area", "0,0,100,100,200,200"},
+    	    {"show", "false"}
+    	};
+            // 带参数调用人流量统计
+            var result = client.BodyNum(image, options);
+            Console.WriteLine(result); 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.InitialDirectory = ".";
+            file.Filter = "所有文件(*.*)|*.*";
+            file.ShowDialog();
+            
+            filepath1 = file.FileName;   //获得文件的绝对路径
+            if (filepath1 == null || filepath1 == "")
+                return;
+            FileStream fs = new FileStream(filepath1, FileMode.Open, FileAccess.Read); //将图片以文件流的形式进行保存
+            BinaryReader br = new BinaryReader(fs);
+            byte[] imgBytesIn = br.ReadBytes((int)fs.Length); //将流读入到字节数组中 二进制数组
+            MemoryStream ms = new MemoryStream(imgBytesIn);
+            pictureBox3.Image = Image.FromStream(ms);
+
+            var image = File.ReadAllBytes(filepath1);
+            // 调用人体检测与属性识别，可能会抛出网络等异常，请使用try/catch捕获
+            var result = client.Gesture(image);
+            Console.WriteLine(result); 
+
+        }
+    }
+}
