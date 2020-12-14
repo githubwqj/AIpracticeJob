bb6a27ac6484a34e5b58eaf5769915068f544695
githubwqj2 <2812930196@qq.com>
2020-12-14 13:33:21 +0800

    11

--- AnalysisOfTheHuman/AnalysisOfTheHuman/Form1.Designer.cs
+++ AnalysisOfTheHuman/AnalysisOfTheHuman/Form1.Designer.cs
@@ -45,9 +45,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(168, 12);
+            this.button1.Location = new System.Drawing.Point(161, 8);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 34);
+            this.button1.Size = new System.Drawing.Size(81, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "关键点识别";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,18 +57,20 @@
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 52);
+            this.pictureBox1.Location = new System.Drawing.Point(8, 35);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(278, 369);
+            this.pictureBox1.Size = new System.Drawing.Size(234, 350);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(311, 12);
+            this.button2.Location = new System.Drawing.Point(246, 8);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 34);
+            this.button2.Size = new System.Drawing.Size(87, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "人流量统计";
             this.button2.UseVisualStyleBackColor = true;
@@ -77,18 +80,20 @@
             // 
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Default;
-            this.pictureBox2.Location = new System.Drawing.Point(311, 52);
+            this.pictureBox2.Location = new System.Drawing.Point(246, 35);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(361, 134);
+            this.pictureBox2.Size = new System.Drawing.Size(304, 111);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(311, 192);
+            this.button3.Location = new System.Drawing.Point(246, 150);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(131, 34);
+            this.button3.Size = new System.Drawing.Size(87, 23);
             this.button3.TabIndex = 4;
             this.button3.Text = "手势识别";
             this.button3.UseVisualStyleBackColor = true;
@@ -97,9 +102,10 @@
             // pictureBox3
             // 
             this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox3.Location = new System.Drawing.Point(311, 232);
+            this.pictureBox3.Location = new System.Drawing.Point(246, 177);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(208, 189);
+            this.pictureBox3.Size = new System.Drawing.Size(157, 208);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 5;
             this.pictureBox3.TabStop = false;
@@ -107,43 +113,47 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(448, 20);
+            this.label1.Location = new System.Drawing.Point(405, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 18);
+            this.label1.Size = new System.Drawing.Size(47, 12);
             this.label1.TabIndex = 6;
             this.label1.Text = "人流量:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(525, 12);
+            this.textBox1.Location = new System.Drawing.Point(459, 8);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.Size = new System.Drawing.Size(68, 21);
             this.textBox1.TabIndex = 7;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(534, 232);
+            this.label2.Location = new System.Drawing.Point(410, 177);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 18);
+            this.label2.Size = new System.Drawing.Size(77, 12);
             this.label2.TabIndex = 8;
             this.label2.Text = "手势识别结果";
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(537, 253);
+            this.richTextBox1.Location = new System.Drawing.Point(407, 205);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(135, 168);
+            this.richTextBox1.Size = new System.Drawing.Size(143, 180);
             this.richTextBox1.TabIndex = 9;
             this.richTextBox1.Text = "";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 433);
+            this.ClientSize = new System.Drawing.Size(555, 389);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
@@ -154,6 +164,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "人体分析程序";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- AnalysisOfTheHuman/AnalysisOfTheHuman/Form1.cs
+++ AnalysisOfTheHuman/AnalysisOfTheHuman/Form1.cs
@@ -37,29 +37,24 @@ namespace AnalysisOfTheHuman
             filepath1 = file.FileName;   //获得文件的绝对路径
             if (filepath1 == null || filepath1 == "")
                 return;
-            FileStream fs = new FileStream(filepath1, FileMode.Open, FileAccess.Read); //将图片以文件流的形式进行保存
-            BinaryReader br = new BinaryReader(fs);
-            byte[] imgBytesIn = br.ReadBytes((int)fs.Length); //将流读入到字节数组中 二进制数组
-            MemoryStream ms = new MemoryStream(imgBytesIn);
-            pictureBox1.Image = Image.FromStream(ms);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Image = Image.FromFile(filepath1);
             var image = File.ReadAllBytes(filepath1);
             var result = client.BodyAnalysis(image);
-            var person_pix=(JObject)((JArray)result["person_info"])[0];
-            Graphics g = pictureBox1.CreateGraphics();//创建一个画板
-            IEnumerable<JProperty> properties = person_pix.Properties();
-            g.FillEllipse(Brushes.Red, 10, 10, 4, 4);
-            var b = person_pix.PropertyValues();
-            var c = person_pix.HasValues;
-            foreach (JToken item in properties)
-            {
-                while (item.HasValues) { 
-               var value = (JObject)item.Next;
-               g.FillEllipse(Brushes.Red, Convert.ToInt32(value["x"]), Convert.ToInt32(value["y"]), 4, 4);
+            var person_pix_arr=(JArray)result["person_info"];
+            var bmp=new Bitmap(pictureBox1.Image);
+            Graphics g = Graphics.FromImage(bmp);//创建一个画板
+            foreach (JObject person_pix in person_pix_arr) {
+                foreach (var item in (JObject)person_pix["body_parts"])
+                {
+                    var x = item.Value["x"];
+                    var y = item.Value["y"];
+                    g.DrawEllipse(new Pen(Color.Red, 5), Convert.ToInt32(x), Convert.ToInt32(y), 4, 4);
                 }
             }
-            
+            pictureBox1.Image = bmp;
                 
-            Console.WriteLine(result); 
+            
 
         }
 
@@ -77,15 +72,20 @@ namespace AnalysisOfTheHuman
             BinaryReader br = new BinaryReader(fs);
             byte[] imgBytesIn = br.ReadBytes((int)fs.Length); //将流读入到字节数组中 二进制数组
             MemoryStream ms = new MemoryStream(imgBytesIn);
+            //pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox2.Image = Image.FromStream(ms);
+            var bmp = new Bitmap(pictureBox2.Image);
+            //Graphics g = Graphics.FromImage(bmp);//创建一个画板
             var image = File.ReadAllBytes(filepath1);
             var options = new Dictionary<string, object>{
-    	    {"area", "0,0,100,100,200,200"},
-    	    {"show", "false"}
+    	    
+    	    {"show", "true"}
     	};
             // 带参数调用人流量统计
             var result = client.BodyNum(image, options);
-            Console.WriteLine(result); 
+            textBox1.Text = Convert.ToString(result["person_num"]);
+
+           // Console.WriteLine(result); 
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -107,7 +107,17 @@ namespace AnalysisOfTheHuman
             var image = File.ReadAllBytes(filepath1);
             // 调用人体检测与属性识别，可能会抛出网络等异常，请使用try/catch捕获
             var result = client.Gesture(image);
-            Console.WriteLine(result); 
+            StringBuilder sb = new StringBuilder("");
+            foreach (var chars in (JArray)result["result"])  //循环获取值
+            {
+                JObject jo = (JObject)chars;
+                sb.Append("classname:" + Convert.ToString(jo["classname"]) + "\n");
+                sb.Append("probability:" + Convert.ToString(jo["probability"]) + "\n\n\n");
+
+
+            }
+            richTextBox1.Text = sb.ToString();
+           // Console.WriteLine(result); 
 
         }
     }
